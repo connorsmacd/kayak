337e90ed704285936bc83591f3e658ea106bcdc0
Connor S. MacDonald <csmacdonald.audio@gmail.com>
2025-06-07 21:27:11 -0400

    Add std::array type description

---
+++ include/kayak/std/array.hpp
@@ -0,0 +1,19 @@
+#ifndef KAYAK_STD_ARRAY_HPP
+#define KAYAK_STD_ARRAY_HPP
+
+#include <kayak/type_description.hpp>
+
+#include <array>
+
+namespace kayak
+{
+template <typename T, std::size_t N>
+struct type_description<std::array<T, N>>
+  : type_description_impl<"std::array<" + name_of<T> + ", " + as_fixed_string<N>
+                          + ">",
+                          std::array<T, N>>
+{
+};
+} // namespace kayak
+
+#endif
--- test/std.cpp
+++ test/std.cpp
@@ -1,8 +1,15 @@
+#include <kayak/std/array.hpp>
 #include <kayak/std/optional.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 
 using namespace kayak::fixed_string_literals;
+
+TEST_CASE("std::array type_description", "[std::array]")
+{
+  STATIC_REQUIRE(kayak::name_of<std::array<int, 42>>
+                 == "std::array<int, 42>"_fs);
+}
 
 TEST_CASE("std::optional type_description", "[std::optional]")
 {
